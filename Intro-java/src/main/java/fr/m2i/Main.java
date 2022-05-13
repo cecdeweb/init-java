b188a20ab97151de87328a7e0a0e24ad0a52816a
Cécile Lafont <cecilelafont33@gmail.com>
2022-05-13 17:53:36 +0200

    reinit sandbox

--- Intro-java/src/main/java/fr/m2i/Main.java
+++ Intro-java/src/main/java/fr/m2i/Main.java
@@ -1,18 +1,9 @@
 package fr.m2i;
 
-import java.util.InputMismatchException;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
 
-        Apple a = new Apple();
-        a.taste();
-        a.miam();
 
-        PineApple b = new PineApple();
-        b.taste();
-        b.miam();
 
     }
 }
