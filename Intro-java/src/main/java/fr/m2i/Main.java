45ec1f443a33a690c35079959e7a80affcbb6a70
Cécile Lafont <cecilelafont33@gmail.com>
2022-05-16 07:59:14 +0200

    init sandbox

--- Intro-java/src/main/java/fr/m2i/Main.java
+++ Intro-java/src/main/java/fr/m2i/Main.java
@@ -3,16 +3,6 @@ package fr.m2i;
 public class Main {
     public static void main(String[] args) {
 
-        Wizard wizard = new Wizard();
-        //wizard.getHealthPoint()=10;
-        System.out.println(wizard.speak());
-        wizard.ennemy(2); // attack points
-
-/*
-        System.out.println(wizard.attack());
-        System.out.println("getHealthPoint");
-        System.out.println(wizard.healtEnnemy);
-*/
 
     }
 
