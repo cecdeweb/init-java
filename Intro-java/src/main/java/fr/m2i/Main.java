618ee2fd10bb31e4d29f9998d6dabf7ec84849a5
Cécile Lafont <cecilelafont33@gmail.com>
2022-05-16 14:04:11 +0200

    exBrawlerCharacter : correction in sandbox

--- Intro-java/src/main/java/fr/m2i/Main.java
+++ Intro-java/src/main/java/fr/m2i/Main.java
@@ -1,8 +1,21 @@
 package fr.m2i;
 
 public class Main {
+
     public static void main(String[] args) {
 
+        Soldier matteo = null;
+        Wizard cecile = new Wizard("Cécile", 89, 95, 68);
 
+        cecile.move();
+        cecile.attack(matteo);
+
+/*
+        Soldier matteo = new Soldier("Mettéo", 100, 78, 45);
+        Wizard cecile = new Wizard("Cécile", 89, 95, 68);
+
+        cecile.move();
+*/
+        cecile.attack(matteo);
     }
 }
