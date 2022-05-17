977fe0af1bb5b50b8184e15a92d48ee1f7594c40
Cécile Lafont <cecilelafont33@gmail.com>
2022-05-17 11:24:59 +0200

    ja-ex1 : add result

--- Intro-java/src/main/java/fr/m2i/Main.java
+++ Intro-java/src/main/java/fr/m2i/Main.java
@@ -1,9 +1,19 @@
 package fr.m2i;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        double rayon;
+        double surface = 0;
+        double pi = 3.14;
 
+        System.out.print("Entrez le rayon du cercle : ");
+        rayon = scanner.nextInt();
+        surface = pi * rayon * rayon;
+        System.out.println("La surface du cercle est : "+surface);
 
     }
 }
