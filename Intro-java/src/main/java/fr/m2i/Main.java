409f6451fa2401fb3427b534c45bd510ee585e31
Cécile Lafont <cecilelafont33@gmail.com>
2022-05-17 15:12:45 +0200

    ja-ex5 : add deb ex

--- Intro-java/src/main/java/fr/m2i/Main.java
+++ Intro-java/src/main/java/fr/m2i/Main.java
@@ -1,7 +1,34 @@
 package fr.m2i;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        /*
+         * Ecrivez un programme Java qui permet de résoudre une équation du 2nd degré de la forme ax2+bx+c = 0.
+         * L'utilisateur devra fournir a,b,c à partir du clavier, ensuite le programme lui donnera la solution
+         * Principe du fonctionnement d'une équation du 2nd degré:
+         * https://www.maths-et-tiques.fr/telech/Secondegre2ESL.pdf
+         * */
+
+        Scanner scan = new Scanner(System.in);
+
+        int a, b, c;
+        int delta;
+
+        System.out.println("Donnez la valeur de >>> a <<< dans l'équation ax2 + bx + c : ");
+        a=scan.nextInt();
+        System.out.println("Donnez la valeur de >>> b <<< dans l'équation ax2 + bx + c : ");
+        b=scan.nextInt();
+        System.out.println("Donnez la valeur de >>> c <<< dans l'équation ax2 + bx + c : ");
+        c=scan.nextInt();
+        delta=(b*b)-(4*a*c);
+
+        if(delta<0){
+
+        }
+
 
 
     }
