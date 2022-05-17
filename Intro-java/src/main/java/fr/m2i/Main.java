33c61c6b295593555dd84e3db8ec651a43717716
Cécile Lafont <cecilelafont33@gmail.com>
2022-05-17 12:06:57 +0200

    Ãja-ex2 : result

--- Intro-java/src/main/java/fr/m2i/Main.java
+++ Intro-java/src/main/java/fr/m2i/Main.java
@@ -1,8 +1,27 @@
 package fr.m2i;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+/***
+ * Ecrire un programme Java qui demande à l'utilisateur de saisir son nom,
+ * prenom et age, et qui ensuite va afficher le message : "Vous vous
+ * appelez <prenom> <nom>, et vous avez <age> ans"
+ ***/
+        Scanner scanner = new Scanner(System.in);
 
+        String name = "";
+        String firstname = "";
+        int age = 0;
+
+        System.out.println("Quel est votre nom ?");
+        name = scanner.nextLine();
+        System.out.println("Quel est votre prénom ?");
+        firstname = scanner.nextLine();
+        System.out.println("Quel est votre age ?");
+        age = scanner.nextInt();
+        System.out.println("Vous vous appelez " + firstname + " " + name + ", et vous avez " + age + " ans");
 
     }
 }
