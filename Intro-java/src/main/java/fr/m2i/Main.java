751f4b58c2d196e0d8ee73386910421adf2927e4
Cécile Lafont <cecilelafont33@gmail.com>
2022-05-17 11:51:27 +0200

    ja-ex1 : add ennounce

--- Intro-java/src/main/java/fr/m2i/Main.java
+++ Intro-java/src/main/java/fr/m2i/Main.java
@@ -4,16 +4,24 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+/**
+*  Ecrire un programme Java qui demande à l'utilisateur de saisir la valeur du diamètre, et qui ensuite va effectuer le calcul de la surface.
+*  Surface = Rayon x Rayon x PI;
+*  Rayon = Diamètre / 2
+**/
         Scanner scanner = new Scanner(System.in);
 
         double rayon;
         double surface = 0;
-        double pi = 3.14;
+        double pi = Math.PI;
 
         System.out.print("Entrez le rayon du cercle : ");
         rayon = scanner.nextInt();
         surface = pi * rayon * rayon;
         System.out.println("La surface du cercle est : "+surface);
+
+        // voir pour afficher un float à 2 décimal en sortie
 
     }
 }
