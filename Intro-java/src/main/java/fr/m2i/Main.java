555f1f7bc8d8dd639abb39531c39fd636699f503
Cécile Lafont <cecilelafont33@gmail.com>
2022-05-18 13:43:51 +0200

    add cours Arrays and for i

--- Intro-java/src/main/java/fr/m2i/Main.java
+++ Intro-java/src/main/java/fr/m2i/Main.java
@@ -1,38 +1,59 @@
 package fr.m2i;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+        var input=new Scanner(System.in);
+        System.out.print("Combien d'employés souhaitez-vous enregistrer?");
+        var nombreEmployes=input.nextInt();
 
-        String mdp = "motDepasse8!";
-        String email = "email@email.fr";
-        String mdpSaisi = "";
-        String emailSaisi = "";
+        Employe[] employes=new Employe[nombreEmployes];
+        employes[0]=new Employe(1,"Christian","Lisangola");
 
-        for (int i = 1; i <=5 ; i++) {
+        for(int i=0;i<employes.length;i++){
+            System.out.print("Id : ");
+            int id=input.nextInt();
+            // efface memoire next
+            input.nextLine();
 
-            int cpt = 5-i;
+            System.out.print("Votre nom : ");
+            String nom=input.nextLine();
 
-            System.out.println("Saisir votre email :");
-            emailSaisi=scan.nextLine();
-            System.out.println("Saisir votre mot de passe :");
-            mdpSaisi=scan.nextLine();
 
-            if (!mdp.equals(mdpSaisi) || !email.equals(emailSaisi)){
-                if(cpt==0){
-                        System.out.println("Vous avez saisi des mauvais identifiants 5 fois, votre compte est bloqué");
-                        break;
-                }
-            System.out.println("Identifiants incorrect - plus que " + cpt + " essai(s)");
-            }else{
-                System.out.println("Bienvenu dans votre espace client");
-                break;
-            }
+            System.out.print("Votre prenom : ");
+            String prenom=input.nextLine();
 
+//          Employe employe=new Employe(id,prenom,nom);
+//          employes[i]=employe;
+
+            employes[i]=new Employe(id,prenom,nom);
         }
 
 
+//      Affichage de tous les employés
+        System.out.println(Arrays.toString(employes));
+
+//      Affichage de chaque employé de manière individuelle : Mauvaise pratique
+        System.out.println("Employé[0] : "+employes[0]);
+
+//      Affichage de chaque employé de manière individuelle : Bonne pratique
+        for(int i=0;i<employes.length;i++){
+//          System.out.println("Employé["+i+"] : "+employes[i]);
+            System.out.println("Employé["+i+"] : "+employes[i].getLastName());
+        }
+
+//      Simple liste
+//        String[] fruits=new String[3];
+//        fruits[0]="pommes";
+//        fruits[1]="bananes";
+//        fruits[2]="oranges";
+//
+//        System.out.println(Arrays.toString(fruits));
+
+//        Employe chris=new Employe(3,"Jean","Zozor");
+//        System.out.println(chris);
     }
 }
